7bbf834825722013629c2eb09db75968dd8f8e38
Daniel Perez <dperezg23@outlook.com>
2024-12-19 15:59:44 -0500

    comentario

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import Providers from "@/providers/providers";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -19,10 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={`${GeistSans.className} ${GeistMono.className}`}>
+      <body className="antialiased">
         <Providers>
           {children}
         </Providers>
